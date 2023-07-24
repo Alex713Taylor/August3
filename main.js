9e445c8acc2bab5d25108f84012448b07222da03
mscobey <scobey.mark@gmail.com>
2023-07-23 21:22:40 -0500

    test

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 
-window.addEventListener("load",()=>{const form = document.getElementById("dateForm");
+const btn = document.getElementById("submit");
 
-form.addEventListener("submit",async (e)=>{
+btn.addEventListener("click",async (e)=>{
   e.preventDefault();
+  let foods = document.querySelector('input[name="dinns"]:checked').value;
+  let funs = document.querySelector('input[name="funns"]:checked').value;
+  let desserts = document.querySelector('input[name="dess"]:checked').value;
+  let dateT = document.getElementById("datet").value;
+  let hands = document.querySelector('input[name="handshold"]:checked').value;
+  //let valueArr = [foods,"",funs,"",desserts,"",dateT,handshold];
+  
 
-    const data = new FormData(e.target);
-
-   const value = Object.fromEntries(data.entries());
-
-    console.log(JSON.stringify(value));
-   await fetch("http://127.0.0.1:8090/api/collections/date/records", {
+  var value =`{"dinns":"${foods}","othdinn":"","funns":"${funs}","othfunn":"","dess":"${desserts}","othdess":"","datet":"${dateT}","handshold":"${hands}"}`;
+console.log("test");
+   console.log(value);
+  await fetch("http://127.0.0.1:8090/api/collections/date/records", {
   method: "POST",
-  body: JSON.stringify(value),
+  body: value,
   headers: {
     "Content-type": "application/json; charset=UTF-8"
   }
 });
-    form.reset();
-});
+  window.location.reload();
 });
