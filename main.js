77474eeef206224a1d648ada977b5f289ce96a51
mscobey <scobey.mark@gmail.com>
2023-07-23 21:26:52 -0500

    yt

--- main.js
+++ main.js
@@ -23,3 +23,25 @@ console.log("test");
 });
   window.location.reload();
 });
+btn.addEventListener("touchstart",async (e)=>{
+  e.preventDefault();
+  let foods = document.querySelector('input[name="dinns"]:checked').value;
+  let funs = document.querySelector('input[name="funns"]:checked').value;
+  let desserts = document.querySelector('input[name="dess"]:checked').value;
+  let dateT = document.getElementById("datet").value;
+  let hands = document.querySelector('input[name="handshold"]:checked').value;
+  //let valueArr = [foods,"",funs,"",desserts,"",dateT,handshold];
+  
+
+  var value =`{"dinns":"${foods}","othdinn":"","funns":"${funs}","othfunn":"","dess":"${desserts}","othdess":"","datet":"${dateT}","handshold":"${hands}"}`;
+console.log("test");
+   console.log(value);
+  await fetch("http://127.0.0.1:8090/api/collections/date/records", {
+  method: "POST",
+  body: value,
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+});
+  window.location.reload();
+});
